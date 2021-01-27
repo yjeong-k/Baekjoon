cafc752ad9d42855418d826091b694253533e530
yjeong-k <kyj219@snu.ac.kr>
2021-01-27 11:45:47 +0900

    Remove code for assigning values to the board

--- Bronze II/src/Prob1100.java
+++ Bronze II/src/Prob1100.java
@@ -8,25 +8,19 @@ public class Prob1100 {
 
         for(int i=0; i<8; i++){
             String chess = sc.nextLine();
-            for(int j=0; j<8; j++){
-                board[i][j] = chess.charAt(j);
-            }
-        }
-        sc.close();
-
-        for(int i=0; i<8; i++){
             if(i%2==0){
                 for(int j=0; j<8; j+=2){
-                    if(board[i][j]=='F')
+                    if(chess.charAt(j)=='F')
                         result++;
                 }
             } else{
                 for(int j=1; j<8; j+=2){
-                    if(board[i][j]=='F')
+                    if(chess.charAt(j)=='F')
                         result++;
                 }
             }
         }
+        sc.close();
 
         System.out.println(result);
     }
